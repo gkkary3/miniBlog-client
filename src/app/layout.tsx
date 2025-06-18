9e41aa8efd578ca79d894a06a8228eef68231725
gkkary3 <nambawon1@naver.com>
2025-06-18 17:11:51 +0900

    MDEditor update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "@uiw/react-md-editor/markdown-editor.css";
+import "@uiw/react-markdown-preview/markdown.css";
 import Header from "@/components/Header";
 import QueryProvider from "./QueryProvider";
 import AuthInitializer from "@/components/AuthInitializer";
@@ -16,8 +18,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "MiniBlog",
-  description: "미니 블로그",
+  title: "Boolog",
+  description: "개발자를 위한 블로그 플랫폼",
 };
 
 export default function RootLayout({
@@ -26,7 +28,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ko">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
