8455bc2c675b641d8e5efcce3201f0ce56b95c38
gkkary3 <nambawon1@naver.com>
2025-06-18 18:28:44 +0900

    MDEditor dynamic change

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
+/** @type {import('next').NextConfig} */
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: "javascript/auto",
+    });
+    return config;
+  },
   /* config options here */
 };
 
